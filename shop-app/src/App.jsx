40dcf4b7989d822eae13b81d538c5ed9dab5feae
Brasero <brandonricci4@gmail.com>
2025-01-24 09:06:39 +0100

    début de correction

---
+++ shop-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from "./component/Header/index.jsx";
+
+function App() {
+ 
+  return (
+    <>
+     <Header />
+    </>
+  )
+}
+
+export default App
---
+++ shop-app/src/component/Header/index.jsx
@@ -0,0 +1,14 @@
+// path: shop-app/src/component/Header/index.jsx
+import "./style.scss";
+
+const Header = () => {
+ 
+ return (
+  <header className="Header">
+    <h1>Shop App</h1>
+      <p>Tout ce dont vous avez besoin, dans un seul endroit</p>
+  </header>
+ )
+}
+
+export default Header
