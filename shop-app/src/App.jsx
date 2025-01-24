2a4f0d9e29683987aee2b7ecf58a26793b4815df
Brasero <brandonricci4@gmail.com>
2025-01-24 15:56:40 +0100

    proposition de correction

--- shop-app/src/App.jsx
+++ shop-app/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from "./component/Header/index.jsx";
+import HomePage from "./component/Home/index.page.jsx";
 
 function App() {
  
   return (
-    <>
+    <div className={'app'}>
      <Header />
-    </>
+     <HomePage />
+    </div>
   )
 }
 
--- shop-app/src/component/Header/index.jsx
+++ shop-app/src/component/Header/index.jsx
@@ -1,14 +1,14 @@
 // path: shop-app/src/component/Header/index.jsx
 import "./style.scss";
+import Banner from "../Banner/index.jsx";
 
 const Header = () => {
  
- return (
-  <header className="Header">
-    <h1>Shop App</h1>
-      <p>Tout ce dont vous avez besoin, dans un seul endroit</p>
-  </header>
- )
+ return (<header className="Header">
+   <h1>Shop App</h1>
+   <p>Tout ce dont vous avez besoin, au même endroit</p>
+   <Banner/>
+  </header>)
 }
 
 export default Header
